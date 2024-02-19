02756adf1165fefe95e7926ccc54478f8fba9f51
mdoluashvili <git config --global user.name>
2024-02-19 16:47:50 +0400

    new branch

---
+++ src/components/Authorization/Authorization.tsx
@@ -0,0 +1,6 @@
+"use client";
+import React from "react";
+
+export default function Authorization() {
+  return <div className="bgImage">mari</div>;
+}
---
+++ src/components/Authorization/AuthorizationStyled.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+interface Button {
+  variant?: string;
+}
+const StyledButton = styled.button<Button>`
+  border: 2px solid #4caf50;
+  background-color: ${(props) =>
+    props.variant === "outline" ? "#fff" : "#4caf50"};
+  color: ${(props) => (props.variant === "outline" ? "#fff" : "#z")};
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  cursor: pointer;
+  transition: 0.5s all ease-out;
+`;
+export default StyledButton;
